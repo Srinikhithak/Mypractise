d040a8591e00226b6e3ce6793629aff08cc03e0f
Srinikithak <bkondred@iu.edu>
2022-06-01 18:54:05 -0400

    Updated code

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 print("Hello")
-for i in range(0,5)
-    if(i %2 == 0)
-        print ("even")
-    else
-        print ("odd")
+for i in range(1,5):
+    if(i %2 == 0):
+        print (i," is even")
+    else:
+        print (i," is odd")
